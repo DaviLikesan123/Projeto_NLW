c079622353fe62bf1b09d769745f15d616561e99
DaviLikesan123 <davidlimboksiok@gmail.com>
2021-04-20 16:38:33 -0300

    Dia 2 NLW

---
+++ lib/home/widgets/app_bar/app_bar_widget.dart
@@ -0,0 +1,47 @@
+import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/cupertino.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 161,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                  text: "Dave Dev",
+                                  style: AppTextStyles.titleBold)
+                            ])),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://instagram.fgru4-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83850905_268312514133356_7683718611625574400_n.jpg?tp=1&_nc_ht=instagram.fgru4-1.fna.fbcdn.net&_nc_ohc=3vprctLUX-sAX8Tecxr&edm=ABfd0MgAAAAA&ccb=7-4&oh=2674059b2e38b2c2057b8e52b95b2e2f&oe=60A1CDB4&_nc_sid=7bff83"))),
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
+                ],
+              ),
+            ));
+}
